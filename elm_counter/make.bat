4c1c2f498651e5cd5566e8cd19a5d81cd69c0d37
Andre Alexander Bell <github@andre-bell.de>
2018-12-17 14:50:57 +0100

    Adding compiled elm code
    * Adding compiled elm code
    * uglifyjs for smaller assett

--- elm_counter/make.bat
+++ elm_counter/make.bat
@@ -1,2 +1,5 @@
 elm make --output=elm_counter.js --optimize src/Main.elm
+
+uglifyjs --output=elm_counter.js --compress "pure_funcs='F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9',pure_getters,keep_fargs=false,unsafe_comps,unsafe" elm_counter.js
+uglifyjs --output=elm_counter.js --mangle elm_counter.js
 pause
